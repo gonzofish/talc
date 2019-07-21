02e53208a2365a16a8947df4dc8bce50ed8e6778
Matt Fehskens <matt.fehskens@gmail.com>
2019-07-20 23:13:38 -0400

    Create a files utility for handling fs-related functionality

---
+++ test/utils/files.util.test.js
@@ -0,0 +1,18 @@
+const test = require('ava').default;
+const fs = require('fs');
+const path = require('path');
+
+const files = require('../../lib/utils/files.util');
+
+test('#findRoot should find the nearest directory with a package.json', (t) => {
+  const projRoot = process.cwd();
+
+  console.info('toot:', files.findRoot());
+  t.is(files.findRoot(), projRoot);
+
+  process.chdir(path.join(projRoot, 'test'));
+
+  t.is(files.findRoot(), projRoot);
+
+  process.chdir(projRoot);
+});
