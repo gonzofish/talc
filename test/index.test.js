9b2483d0dd1639bb15882305c0795ccd0f8d0859
Matt Fehskens <matt.fehskens@gmail.com>
2019-07-24 20:13:19 -0400

    Add a publish command

--- test/index.test.js
+++ test/index.test.js
@@ -3,26 +3,37 @@ const sinon = require('sinon');
 
 const operators = require('../lib/operators');
 const run = require('../lib');
+const config = {
+  input: 'posts-md',
+  output: 'posts-html',
+};
 
-test('should delegate a "new" command', (t) => {
-  const config = {
-    input: {
-      dir: 'posts-md',
-      nameFormat: '[date]-[name]',
-    },
-    output: {
-      dir: 'posts-html',
-      nameFormat: '[date]-[name]',
-    },
-  };
+const setup = () => {
   const sandbox = sinon.createSandbox();
-  const generate = sandbox.stub(operators, 'generateMarkdown');
 
   sandbox.stub(operators, 'loadConfig').returns(config);
+
+  return sandbox;
+};
+
+test('should delegate a "new" command to the generator', (t) => {
+  const sandbox = setup();
+  const generate = sandbox.stub(operators, 'generateMarkdown');
 
   run('new', 'This is My Title');
 
   t.true(generate.calledWith('This is My Title', config));
+
+  sandbox.restore();
+});
+
+test('should delegate a "publish" command to the convertor', (t) => {
+  const sandbox = setup();
+  const publish = sandbox.stub(operators, 'convert');
+
+  run('publish');
+
+  t.true(publish.calledWith(config));
 
   sandbox.restore();
 });
