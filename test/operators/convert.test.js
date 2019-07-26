37f25b8206d1d8d9af81a28e8f8b470654c49c3c
Matt Fehskens <matt.fehskens@gmail.com>
2019-07-25 20:42:06 -0400

    Added a test for using a template
    Also created common setup for convert tests

--- test/operators/convert.test.js
+++ test/operators/convert.test.js
@@ -1,22 +1,21 @@
 const test = require('ava').default;
 const sinon = require('sinon');
+
+const cheerio = require('cheerio');
 const showdown = require('showdown');
 
 const files = require('../../lib/utils/files.util');
 const convert = require('../../lib/operators/convert');
 
-test('should convert a markdown file to HTML via showdown', (t) => {
+const setup = () => {
   const sandbox = sinon.createSandbox();
-  const config = {
-    input: 'input',
-    output: 'output',
-  };
   const converterMock = {
     makeHtml: sinon.spy((contents) => `HTML\n\n${contents}`),
   };
-  const Converter = sandbox.stub(showdown, 'Converter').returns(converterMock);
-  const writeFiles = sandbox.stub(files, 'writeFiles');
-  const readFiles = sandbox.stub(files, 'readFiles').returns([
+
+  sandbox.stub(showdown, 'Converter').returns(converterMock);
+  sandbox.stub(files, 'writeFiles');
+  sandbox.stub(files, 'readFiles').returns([
     {
       contents: '# File #1',
       filename: 'file1.md',
@@ -31,12 +30,25 @@ test('should convert a markdown file to HTML via showdown', (t) => {
     },
   ]);
 
+  return {
+    converterMock,
+    sandbox,
+  };
+};
+
+test('should convert a markdown file to HTML via showdown', (t) => {
+  const { sandbox } = setup();
+  const config = {
+    input: 'input',
+    output: 'output',
+  };
+
   convert(config);
 
-  t.true(Converter.calledWith());
-  t.true(readFiles.calledWith('input', 'md'));
+  t.true(showdown.Converter.calledWith());
+  t.true(files.readFiles.calledWith('input', 'md'));
   t.true(
-    writeFiles.calledWith('output', [
+    files.writeFiles.calledWith('output', [
       {
         contents: 'HTML\n\n# File #1',
         filename: 'file1.html',
@@ -55,4 +67,61 @@ test('should convert a markdown file to HTML via showdown', (t) => {
   sandbox.restore();
 });
 
-// NEED TO PARSE YAML metadata
+test('should insert HTML contents into a template, if one exists', (t) => {
+  const { sandbox } = setup();
+  const config = {
+    input: 'input',
+    output: 'output',
+    template: 'my-template.html',
+  };
+  const template = '<html><body>my template</body></html>';
+  const mockCheerioChain = {
+    contents: sinon.spy(() => mockCheerioChain),
+    filter: sinon.spy(() => mockCheerioChain),
+    replaceWith: sinon.spy(),
+  };
+  const mockCheerio = sinon.spy(() => mockCheerioChain);
+
+  mockCheerio.html = sinon.spy(() => `CHEERIO! ${mockCheerio.html.callCount}`);
+  // mockCheerioChain.contents = sinon.spy(() => mockCheerioChain);
+  // mockCheerioChain.filter = sinon.spy(() => mockCheerioChain);
+  // mockCheerioChain.replaceWith = sinon.spy();
+
+  sandbox.stub(cheerio, 'load').returns(mockCheerio);
+  sandbox.stub(files, 'readFile').returns(template);
+
+  convert(config);
+
+  t.true(files.readFile.calledWith('my-template.html'));
+  t.true(cheerio.load.calledWith(template));
+  t.true(mockCheerio.calledWith('*'));
+  t.is(mockCheerioChain.contents.callCount, 3);
+  t.is(mockCheerioChain.filter.callCount, 3);
+  t.true(mockCheerioChain.replaceWith.calledWith('HTML\n\n# File #1'));
+  t.true(mockCheerioChain.replaceWith.calledWith('HTML\n\n# File Ehhhhh'));
+  t.true(
+    mockCheerioChain.replaceWith.calledWith('HTML\n\n# Sleepy Time\n\nZzz'),
+  );
+  t.true(
+    files.writeFiles.calledWith('output', [
+      {
+        contents: 'CHEERIO! 1',
+        filename: 'file1.html',
+      },
+      {
+        contents: 'CHEERIO! 2',
+        filename: 'fileA.html',
+      },
+      {
+        contents: 'CHEERIO! 3',
+        filename: 'Zzz.html',
+      },
+    ]),
+  );
+
+  sandbox.restore();
+});
+
+// NEED TO PARSE YAML metadata to use as variables
+// variables will be talc:<variable>
+// understood vars: title, date, tags (comma-separated)
