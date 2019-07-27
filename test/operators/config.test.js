f598701b5bdc758f75e3b02f93c0b59660d76640
Matt Fehskens <matt.fehskens@gmail.com>
2019-07-27 15:12:56 -0400

    See #1: User config recognizes an `index` attribute

--- test/operators/config.test.js
+++ test/operators/config.test.js
@@ -18,6 +18,7 @@ test.after(() => {
 
 test('should look for a config file next to the nearest package.json', (t) => {
   userConfig = {
+    index: 'my-index.html',
     input: 'posts',
     output: 'published',
     template: 'my-template.html',
@@ -29,6 +30,7 @@ test('should look for a config file next to the nearest package.json', (t) => {
 test('should use a default config if one is not present', (t) => {
   userConfig = undefined;
   t.deepEqual(load(), {
+    index: null,
     input: 'input',
     output: 'output',
     template: null,
@@ -41,6 +43,7 @@ test('should use a partial config', (t) => {
   };
 
   t.deepEqual(load(), {
+    index: null,
     input: 'pizza',
     output: 'output',
     template: null,
