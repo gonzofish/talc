d0b78997adb58bfa10cb4892d3e10b5b8204952d
Matt Fehskens <matt.fehskens@gmail.com>
2019-08-11 10:47:04 -0400

    Test command aliases

--- test/index.test.js
+++ test/index.test.js
@@ -27,11 +27,33 @@ test('should delegate a "new" command to the generator', (t) => {
   sandbox.restore();
 });
 
+test('should alias "new" with "n"', (t) => {
+  const sandbox = setup();
+  const generate = sandbox.stub(operators, 'generateMarkdown');
+
+  run('n', 'Aliased new');
+
+  t.true(generate.calledWith('Aliased new', config));
+
+  sandbox.restore();
+});
+
 test('should delegate a "build" command to the convertor', (t) => {
   const sandbox = setup();
   const convert = sandbox.stub(operators, 'convert');
 
   run('build');
+
+  t.true(convert.calledWith(config));
+
+  sandbox.restore();
+});
+
+test('should alias "build" with "b"', (t) => {
+  const sandbox = setup();
+  const convert = sandbox.stub(operators, 'convert');
+
+  run('b');
 
   t.true(convert.calledWith(config));
 
