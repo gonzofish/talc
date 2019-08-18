75e48a3739de139cce62a1a0933abccbf99e3ae9
Matt Fehskens <matt.fehskens@gmail.com>
2019-08-18 09:16:17 -0400

    See #3: Make generate use files util to write files

--- test/operators/generate.test.js
+++ test/operators/generate.test.js
@@ -1,9 +1,9 @@
 const test = require('ava').default;
-const fs = require('fs');
 const sinon = require('sinon');
 
 const generate = require('../../lib/operators/generate');
 const datesUtil = require('../../lib/utils/dates.util');
+const filesUtil = require('../../lib/utils/files.util');
 
 test.before(() => {
   sinon.stub(datesUtil, 'getCurrent').returns('2018-08-03 12:34:00');
@@ -19,46 +19,17 @@ test('should create a file using the config options', (t) => {
   };
   const filename = 'my_drafts/my-first-post.md';
   const sandbox = sinon.createSandbox();
-  const writeFile = sandbox.stub(fs, 'writeFileSync');
+  const writeFile = sandbox.stub(filesUtil, 'writeFiles');
   const contents = `---
 title: My First Post
 create_date: 2018-08-03 12:34:00
 ---
-# My First Post
 
 `;
-
-  sandbox.stub(fs, 'existsSync').returns(true);
 
   generate('My First Post', config);
 
-  t.true(writeFile.calledWith(filename, contents));
-
-  sandbox.restore();
-});
-
-test('should create the input directory if it does not exist', (t) => {
-  const config = {
-    drafts: 'my_posts',
-  };
-  const filename = 'my_posts/my-first-entry.md';
-  const sandbox = sinon.createSandbox();
-  const writeFile = sandbox.stub(fs, 'writeFileSync');
-  const mkdir = sandbox.stub(fs, 'mkdirSync');
-  const contents = `---
-title: My First Entry
-create_date: 2018-08-03 12:34:00
----
-# My First Entry
-
-`;
-
-  sandbox.stub(fs, 'existsSync').returns(false);
-
-  generate('My First Entry', config);
-
-  t.true(mkdir.calledWith('my_posts'));
-  t.true(writeFile.calledWith(filename, contents));
+  t.true(writeFile.calledWith(filename, [contents]));
 
   sandbox.restore();
 });
