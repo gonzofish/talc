f9f27aca835356a1c3072c0075dc41e9c843e8dd
Matt Fehskens <matt.fehskens@gmail.com>
2022-05-29 09:39:59 -0400

    Remove Luxon, add date-fns
    * Update date utility to work with Date through date-fns
    * Change dateUtil.toLuxon -> dateUtil.convertToDate
    * Update tests to use a fake timer (use to use LuxonSettings.now
    * Convert's compareDates relied on a Luxon method (toMillis), replaced it with the native valueOf
    
    Remove remaining Luxon references
    
    * Config utility was using DateTime, swap out for dates util's `format`
    * Dates util's tests still had DateTime references

--- test/utils/dates.util.test.js
+++ test/utils/dates.util.test.js
@@ -1,36 +1,38 @@
 const test = require('ava').default;
-const { DateTime, Settings: LuxonSettings } = require('luxon');
 const sinon = require('sinon');
 
 const dates = require('../../lib/utils/dates.util');
+let clock;
+let sandbox;
 
 test.before(() => {
-  LuxonSettings.now = () => new Date('2010-03-27 04:30:37').valueOf();
-  // const now = myDate.valueOf();
+  sandbox = sinon.createSandbox();
+  clock = sinon.useFakeTimers(new Date('2010-03-27 04:30:37').valueOf());
+});
 
-  // sinon.useFakeTimers({ now });
-  sinon.stub(DateTime, 'fromJSDate').callThrough();
-  sinon.stub(DateTime, 'local').callThrough();
+test.after(() => {
+  sandbox.restore();
+  clock.restore();
 });
 
 test('#format should take a date string and format it to the provided format', (t) => {
   t.is(dates.format('2018-08-03 08:01:00', 'M/d/yyyy'), '8/3/2018');
 });
 
-test('#getCurrent should provide current date & time', (t) => {
+test.only('#getCurrent should provide current date & time', (t) => {
   t.is(dates.getCurrent(), '2010-03-27 04:30:37');
 });
 
-test('#toLuxon should create a date time from a provided date string', (t) => {
-  const luxonDate = DateTime.local(2018, 8, 3, 8, 1, 0, 0);
-  const result = dates.toLuxon('2018-08-03 08:01:00');
+test('#convertToDate should create a date time from a provided date string', (t) => {
+  const luxonDate = new Date('2018-08-13 08:01:00');
+  const result = dates.convertToDate('2018-08-03 08:01:00');
 
-  t.is(result.toMillis(), luxonDate.toMillis());
+  t.is(result.valueOf(), luxonDate.valueOf());
 });
 
-test('#toLuxon should create a date time from a provided date string using a provided format', (t) => {
-  const luxonDate = DateTime.local(2018, 8, 3, 0, 0, 0, 0);
-  const result = dates.toLuxon('2018-08-03', 'yyyy-MM-dd');
+test('#convertToDate should create a date time from a provided date string using a provided format', (t) => {
+  const date = new Date('2018-08-13 08:01:00');
+  const result = dates.convertToDate('2018-08-03', 'yyyy-MM-dd');
 
-  t.is(result.toMillis(), luxonDate.toMillis());
+  t.is(result.valueOf(), date.valueOf());
 });
